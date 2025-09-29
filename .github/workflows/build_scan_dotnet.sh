685bfc35607b997a7f4119c78c2b23470abecf63
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2025-09-29 12:03:33 +0200

    SLOMNI-62 Analyze on SQS Next

--- .github/workflows/build_scan_dotnet.sh
+++ .github/workflows/build_scan_dotnet.sh
@@ -10,16 +10,52 @@ set -euo pipefail
 VERSION=$(cat scripts/common.cake | grep ' SemVer = "' | sed -nre 's/.*([0-9]+\.[0-9]+\.[0-9]+).*$/\1/p')
 echo "Sonar project version is '${VERSION}'"
 
-REGION_FLAG=$([ "$SONAR_REGION" = "us" ] && echo "-d:sonar.region=$SONAR_REGION" || echo "")
+# Determine if this is a pull request
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+    export PULL_REQUEST="${GITHUB_REF##refs/pull/}"
+    export GITHUB_BRANCH="${GITHUB_HEAD_REF}"
+    export GITHUB_BASE_BRANCH="${GITHUB_BASE_REF}"
+else
+    export PULL_REQUEST="false"
+    export GITHUB_BRANCH="${GITHUB_REF_NAME}"
+fi
 
-# Setup SonarQube scan
-dotnet sonarscanner begin \
-  -d:sonar.token=${SONAR_TOKEN} \
-  $REGION_FLAG \
-  -o:sonarsource \
-  -d:sonar.projectBaseDir="$GITHUB_WORKSPACE/src" \
-  -k:"SonarSource_omnisharp-roslyn" \
-  -v:${VERSION}
+echo "Environment variables set:"
+echo "GITHUB_SHA: $GITHUB_SHA"
+echo "PULL_REQUEST: $PULL_REQUEST"
+echo "GITHUB_RUN_ID: $GITHUB_RUN_ID"
+echo "GITHUB_BRANCH: $GITHUB_BRANCH"
+echo "GITHUB_BASE_BRANCH: ${GITHUB_BASE_REF:-}"
+
+# Build (ideally should also run .NET unit tests but they are failing)
+powershell -File build.ps1 -target Build -configuration Release
+
+SONAR_PARAMS=(
+  -v:"${VERSION}"
+  -k:"SonarSource_omnisharp-roslyn"
+  -o:"sonarsource"
+  -d:sonar.host.url="${SONAR_URL}"
+  -d:sonar.token="${SONAR_TOKEN}"
+  -d:sonar.analysis.pipeline="$GITHUB_RUN_ID"
+  -d:sonar.analysis.sha1="${GITHUB_SHA}"
+  -d:sonar.scanner.scanAll=false
+)
+
+if [ "$GITHUB_BRANCH" == "master" ] && [ "$PULL_REQUEST" == "false" ]; then
+  echo '======= Analyze master branch'
+  dotnet sonarscanner begin "${SONAR_PARAMS[@]}"
+
+elif [ "$PULL_REQUEST" != "false" ]; then
+  echo '======= Analyze pull request'
+  dotnet sonarscanner begin "${SONAR_PARAMS[@]}" \
+    -d:sonar.pullrequest.key="${PULL_REQUEST}" \
+    -d:sonar.pullrequest.branch="${GITHUB_BRANCH}" \
+    -d:sonar.pullrequest.base="${GITHUB_BASE_BRANCH}"
+
+else
+    echo '======= Analyze branch'
+    dotnet sonarscanner begin "${SONAR_PARAMS[@]}" -d:sonar.branch.name="${GITHUB_BRANCH}"
+fi
 
 # Build (ideally should also run .NET unit tests but they are failing)
 powershell -File build.ps1 -target Build -configuration Release
